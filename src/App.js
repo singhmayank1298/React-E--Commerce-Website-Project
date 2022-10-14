9051e13642ff402349d0fbb052e2f856ab5193c0
singhmayank1298 <108435481+singhmayank1298@users.noreply.github.com>
2022-10-14 18:54:09 +0530

    Protecting the product page means store 21

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Summary from "./components/UI/Summary";
 import "./App.css";
@@ -13,9 +13,11 @@ import Home from "./components/Home/Home";
 import Contact from "./components/Contact/Contect";
 import ProductDetailPage from "./components/ProductDetailPage/ProductDetailPage";
 import Login from "./components/Login/Login";
+import CartContext from "./components/store/cart-container";
 
 function App() {
   const [CartVisible, setCartVisible] = useState(false);
+  const ctx = useContext(CartContext);
 
   const CartVisibleHandeler = () => {
     setCartVisible(() => {
@@ -24,45 +26,49 @@ function App() {
   };
 
   return (
-    <CartProvider>
-      <div className="body">
-        <Header onClick={CartVisibleHandeler}></Header>
-        <Summary></Summary>
-        <Switch>
-          <Route path="/about">
-            <About></About>
-          </Route>
+    <div className="body">
+      <Header onClick={CartVisibleHandeler}></Header>
+      <Summary></Summary>
+      <Switch>
+        <Route path="/about">
+          <About></About>
+        </Route>
 
-          <Route path="/home">
-            <Home></Home>
-          </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
 
+        {ctx.userIsLogin && (
           <Route path="/store" exact>
             <StoreItems></StoreItems>
           </Route>
+        )}
 
-          <Route path="/contact">
-            <Contact></Contact>
-          </Route>
+        <Route path="/contact">
+          <Contact></Contact>
+        </Route>
 
-          <Route path="/product">
-            <ProductDetailPage></ProductDetailPage>
-          </Route>
+        <Route path="/product">
+          <ProductDetailPage></ProductDetailPage>
+        </Route>
 
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-        </Switch>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
 
-        <FooterItems></FooterItems>
-        {CartVisible === true
-          ? ReactDOM.createPortal(
-              <CartBox></CartBox>,
-              document.getElementById("Cart")
-            )
-          : ""}
-      </div>
-    </CartProvider>
+        <Route path="/*">
+          <Redirect to="login"></Redirect>
+        </Route>
+      </Switch>
+
+      <FooterItems></FooterItems>
+      {CartVisible === true
+        ? ReactDOM.createPortal(
+            <CartBox></CartBox>,
+            document.getElementById("Cart")
+          )
+        : ""}
+    </div>
   );
 }
 
